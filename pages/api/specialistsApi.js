e5bb43973ebcfabde709ecc20ca529887d39b146
AlexNed32 <37212720+AlexNed32@users.noreply.github.com>
2022-05-05 23:49:40 +0300

    Delete pages directory

--- pages/api/specialistsApi.js
+++
@@ -1,7 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-import { specialists } from "./data/specialistsData"
-
-export default function handler(req, res) {
-        res.status(200).json(specialists)
-}
